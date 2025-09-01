e934de07935882daea17d726fa04f6b61365a4f3
Gabriel-V-Martins <gabriel.martins318.dev@gmail.com>
2025-09-01 19:28:45 -0300

    NEW: implementação da sequência de jogo X ou O

--- src/App.js
+++ src/App.js
@@ -2,29 +2,47 @@
 import { useState } from 'react';
 import './App.css'
 
-function Square(valor, onSquareClick) {
+function Square({ valor, onSquareClick }) {
   return (
-    <button className="square" onClick={onSquareClick}>{valor}</button>
+    <button className="square" onClick={onSquareClick}>
+      {valor}
+    </button>
   );
 }
 
 export default function Tabuleiro() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  function handleClick(i) {
+    if (squares[i]) return; //if squares[i] is null the 'if' does not execute the 'return'!
+    //the 'handle Click' 
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
   return (
     <div>
       <div>
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square valor={squares[0]} onSquareClick={() => { handleClick(0) }} />
+        <Square valor={squares[1]} onSquareClick={() => { handleClick(1) }} />
+        <Square valor={squares[2]} onSquareClick={() => { handleClick(2) }} />
       </div>
       <div>
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square valor={squares[3]} onSquareClick={() => { handleClick(3) }} />
+        <Square valor={squares[4]} onSquareClick={() => { handleClick(4) }} />
+        <Square valor={squares[5]} onSquareClick={() => { handleClick(5) }} />
       </div>
       <div>
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square valor={squares[6]} onSquareClick={() => { handleClick(6) }} />
+        <Square valor={squares[7]} onSquareClick={() => { handleClick(7) }} />
+        <Square valor={squares[8]} onSquareClick={() => { handleClick(8) }} />
       </div>
     </div>
 
