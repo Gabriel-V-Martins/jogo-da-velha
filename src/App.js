2d1366eb2e491dd41adedce5948b14c60d235f75
Gabriel-V-Martins <gabriel.martins318.dev@gmail.com>
2025-09-01 21:33:33 -0300

    NEW: implementada a regra de vencedor

--- src/App.js
+++ src/App.js
@@ -15,8 +15,7 @@ export default function Tabuleiro() {
   const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(i) {
-    if (squares[i]) return; //if squares[i] is null the 'if' does not execute the 'return'!
-    //the 'handle Click' 
+    if (squares[i] || haVencedor(squares)) return alert(haVencedor(squares) + " venceu!");
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
@@ -47,4 +46,52 @@ export default function Tabuleiro() {
     </div>
 
   )
+}
+
+function haVencedor(squares) {
+  // Linhas
+  if (squares[0] === squares[1] && squares[0] === squares[2]) return squares[0];
+  if (squares[3] === squares[4] && squares[3] === squares[5]) return squares[3];
+  if (squares[6] === squares[7] && squares[6] === squares[8]) return squares[6];
+
+  // Colunas
+  if (squares[0] === squares[3] && squares[0] === squares[6]) return squares[0];
+  if (squares[1] === squares[4] && squares[1] === squares[7]) return squares[1];
+  if (squares[2] === squares[5] && squares[2] === squares[8]) return squares[2];
+
+  // Diagonais
+  if (squares[0] === squares[4] && squares[0] === squares[8]) return squares[0];
+  if (squares[2] === squares[4] && squares[2] === squares[6]) return squares[2];
+
+  // if ((squares[0] === squares[1] && squares[0] === squares[2]) ||
+  //   (squares[3] === squares[4] && squares[3] === squares[5]) ||
+  //   (squares[6] === squares[7] && squares[6] === squares[8]) ||
+  //   (squares[0] === squares[3] && squares[0] === squares[6]) ||
+  //   (squares[1] === squares[4] && squares[1] === squares[7]) ||
+  //   (squares[2] === squares[5] && squares[2] === squares[8]) ||
+  //   (squares[0] === squares[4] && squares[0] === squares[8]) ||
+  //   (squares[2] === squares[4] && squares[2] === squares[6])
+  // ) {
+  //   return squares;
+  // }
+
+  // const linha = [
+  //   [0, 1, 2],
+  //   [3, 4, 5],
+  //   [6, 7, 8],
+  //   [0, 3, 6],
+  //   [1, 4, 7],
+  //   [2, 5, 8],
+  //   [0, 4, 8],
+  //   [2, 4, 6]
+  // ];
+
+  // for (let l of linha) {
+  //   const [a, b, c] = l;
+  //   if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+  //     return squares[a];
+  //   }
+  // }
+  return null;
+
 }
